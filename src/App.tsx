f480bc5e86a21b7ad17e5c8f1669c7a9fa53081f
PuNe <57552176+worachanon226@users.noreply.github.com>
2023-06-19 12:15:15 +0700

    Update Numpad

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Numpad from "./components/Numpad";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Numpad color="red" onClick={undefined}>
-          +
-        </Numpad>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/components/Numpad.jsx
+++ src/components/Numpad.jsx
@@ -1,24 +1,25 @@
+import { isLabelWithInternallyDisabledControl } from "@testing-library/user-event/dist/utils";
 import React from "react";
 import styled, { css } from "styled-components";
 
 const colorStyle = (color) => {
-  if (color === "red") {
-    console.log("Red");
+  if (color === "grey") {
     return css`
-      background-color: #c04444;
-      color: #fff;
+      background-color: #c4c4c4;
+      color: #000000;
       &:hover,
       &:focus {
-        background-color: #af3b3b;
+        background-color: #dfdfdf;
       }
     `;
-  } else if (color === "green") {
+  } else if (color === "orange") {
     return css`
-      background-color: #018645;
-      color: #fff;
+      background-color: #ffa05f;
+      color: #ffffff;
       &:hover,
       &:focus {
-        background-color: #016d38;
+        background-color: #ffffff;
+        color: #ffa05f;
       }
     `;
   } else if (color === "dark") {
@@ -42,13 +43,38 @@ const colorStyle = (color) => {
   }
 };
 
+const textStyle = (isLarge) => {
+  if (isLarge) {
+    return css`
+      font-size: 50px;
+    `;
+  } else {
+    return css`
+      font-size: 35px;
+    `;
+  }
+};
+
 export const ButtonStyle = styled.button`
+  font-family: inherit;
+  margin: 10px 10px 10px 10px;
+  border: 1px solid rgba(0, 0, 0, 0.05);
+  border-radius: 50%;
+  height: 100px;
+  width: 100px;
+  font-weight: bold;
+
+  transition: background-color 0.15s ease-in-out, opacity 0.15s ease-in-out;
+  position: relative;
+  overflow: hidden;
+  transform: translate3d(0, 0, 0);
   ${({ color }) => colorStyle(color)}
+  ${({ isLarge }) => textStyle(isLarge)}
 `;
 
-const Numpad = ({ children, color, onClick }) => {
+const Numpad = ({ children, color, isLarge, onClick }) => {
   return (
-    <ButtonStyle color={color} onClick={onClick}>
+    <ButtonStyle color={color} isLarge={isLarge} onClick={onClick}>
       {children}
     </ButtonStyle>
   );
